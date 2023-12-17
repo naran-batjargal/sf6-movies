ab9588321ad57bc4e49d2486dc0036bf1701fde3
test <test@Becks-MacBook-Pro.local>
2023-12-17 17:03:46 -0600

    feat: facebook login is added

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -37,7 +37,7 @@ class AdController extends AbstractController
         $pagination = $this->paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            4 /*limit per page*/
+            6 /*limit per page*/
         );
 
         //dd($pagination);
